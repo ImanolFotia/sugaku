67b50830e63bb22fec3372c357c72a47436ab04e
eaviti <emiliano.a.viti@gmail.com>
2016-11-02 17:23:57 -0300

    Cart

--- Controladora/Cart.js
+++ Controladora/Cart.js
@@ -1,11 +1,9 @@
-//función para agregar filas (rows) por cada producto
-
-
-
-
-
-
-
+//función para agregar una fila y luego texto a esa fila
+function agregarFila(texto){
+	var fila = cart.insertRow(0);
+	var celdaDerecha = fila.insertCell(1);
+	celdaDerecha.innerHTML = texto;
+}
 //función para mostrar u ocultar el cart
 function hideCart(){
 	document.getElementById("TableCart").style.zIndex = -1;
