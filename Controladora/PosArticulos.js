d645517e546591cd90de9e92814ce9f87bed46a1
Imanol Fotia <imanolfotia@gmail.com>
2016-12-09 20:57:48 -0300

    Mejorada renderizacion de Articulo

---
+++ Controladora/PosArticulos.js
@@ -0,0 +1,32 @@
+var PocisionesArticulos = [];
+
+
+PocisionesArticulos[0] = [57, 17, 20];
+PocisionesArticulos[1] = [78, 17, 20];
+PocisionesArticulos[2] = [99, 17, 20];
+PocisionesArticulos[3] = [57, 17, 30];
+PocisionesArticulos[4] = [78, 17, 30];
+PocisionesArticulos[5] = [99, 17, 30];
+
+PocisionesArticulos[6] = [99, 17, 50];
+PocisionesArticulos[7] = [78, 17, 50];
+PocisionesArticulos[8] = [57, 17, 50];
+PocisionesArticulos[9] = [99, 17, 40];
+PocisionesArticulos[10] = [78, 17, 40];
+PocisionesArticulos[11] = [57, 17, 40];
+
+PocisionesArticulos[12] = [57, 17, 60];
+PocisionesArticulos[13] = [78, 17, 60];
+PocisionesArticulos[14] = [99, 17, 60];
+PocisionesArticulos[15] = [57, 17, 70];
+PocisionesArticulos[16] = [78, 17, 70];
+PocisionesArticulos[17] = [99, 17, 70];
+
+var TiposArticulos = [];
+
+
+TiposArticulos[0] = ["Resources/Sprites/Botella de Agua clone.png"];
+TiposArticulos[1] = ["Resources/Sprites/Caramelo 256.png"];
+TiposArticulos[2] = ["Resources/Sprites/Caramelos 256.png"];
+TiposArticulos[3] = ["Resources/Sprites/leche.png"];
+
